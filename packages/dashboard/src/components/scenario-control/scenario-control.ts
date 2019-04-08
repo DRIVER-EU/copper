503ea5dd8ee7d349de0e4da5ce3a2f3511bffa4d
damylen <damylen@gmail.com>
2019-04-08 09:51:54 +0200

    dashboard: updated scenario-control with new timesource

--- packages/dashboard/src/components/scenario-control/scenario-control.ts
+++ packages/dashboard/src/components/scenario-control/scenario-control.ts
@@ -2,37 +2,71 @@ import { WidgetBase, AppState } from '@csnext/cs-client';
 import './scenario-control.css';
 import Component from 'vue-class-component';
 import Vue from 'vue';
+import { WidgetOptions, TimeDataSource, Topics} from '@csnext/cs-core';
 
+export class ScenarioControlOptions extends WidgetOptions {
+  public timesource?: string;
+}
 @Component({
   name: 'scenario-control',
   template: require('./scenario-control.html')
 } as any)
 export class ScenarioControl extends WidgetBase {
-
   public adapterState: any = {};
+  public time: TimeDataSource = new TimeDataSource();
+  public focus = '';
 
   mounted() {
-    if (this.$cs.socket) {
+    if (
+      this.widget.options &&
+      this.widget.options.hasOwnProperty('timesource')
+    ) {
+      this.$cs
+        .loadDatasource<TimeDataSource>(
+          (this.widget.options as ScenarioControlOptions).timesource!
+        )
+        .then(t => {
+          Vue.set(this, 'time', t);
+          this.time.events.subscribe(Topics.TIME_TOPIC, (a: string, d: any) => {
+            Vue.nextTick(() => {
+              Vue.set(this, 'time', this.time);
+              if (this.time.focusTime) {
+                this.focus = this.time.focusTime.toString();
+              }
+            });
+          });
+        })
+        .catch(r => {
+          this.time = new TimeDataSource();
+        });
+    } else {
+      this.time = new TimeDataSource();
+    }
 
-      this.$cs.socket.on('time', (d:any) => {
-        this.adapterState = d;        
+    if (this.$cs.socket) {
+      this.$cs.socket.on('time', (d: any) => {
+        this.adapterState = d;
       });
     }
   }
 }
 
 Vue.filter('formatTime', (value: number) => {
-  if (value !== undefined) {    
+  if (value !== undefined) {
     const date = new Date(value);
-    return ("0" + date.getHours()).slice(-2) + ":" + ("0" + date.getMinutes()).slice(-2) + ":" + ("0" + date.getSeconds()).slice(-2);
+    return (
+      ('0' + date.getHours()).slice(-2) +
+      ':' +
+      ('0' + date.getMinutes()).slice(-2) +
+      ':' +
+      ('0' + date.getSeconds()).slice(-2)
+    );
   }
 });
 
 Vue.filter('formatDay', (value: number) => {
-  if (value !== undefined) {    
+  if (value !== undefined) {
     const date = new Date(value);
     return date.getDay() + '/' + date.getMonth() + '/' + date.getFullYear();
   }
 });
-
-
